046aea06527a3c0e721395d20014da3601d397f8
Arul  Khanna Vannala <41644359+arulvannala@users.noreply.github.com>
2023-10-25 11:30:24 -0600

    Update SecurityConfigurationnew.java

--- src/main/java/cook/SecurityConfigurationnew.java
+++ src/main/java/cook/SecurityConfigurationnew.java
@@ -1,1 +1,33 @@
 
+
+package cook;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("development")
+
+
+public class SecurityConfigurationnew {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+            .and().authorizeRequests()
+                .antMatchers("/api/v1/session/**",
+                             "/swagger-ui/**",
+                             "/api/swagger-ui/**",
+                             "/v3/api-docs/**",
+                             "/actuator/**",
+                             "/readyz",
+                             "/livez",
+                             "/dev/**").permitAll()
+                .anyRequest().authenticated();
+
+    }
+}
