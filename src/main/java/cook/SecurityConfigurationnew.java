6b05c469470f8b5dbcc8900d55f61d880265fd95
Arul  Khanna Vannala <41644359+arulvannala@users.noreply.github.com>
2023-10-25 11:56:01 -0600

    Update SecurityConfigurationnew.java

--- src/main/java/cook/SecurityConfigurationnew.java
+++ src/main/java/cook/SecurityConfigurationnew.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 
 @Configuration
