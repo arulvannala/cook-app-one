a56e1a6d061b08b7149f0fd04ec899a06a3bb63d
Arul  Khanna Vannala <41644359+arulvannala@users.noreply.github.com>
2023-10-25 11:42:29 -0600

    Update SecurityConfigurationnew.java

--- src/main/java/cook/SecurityConfigurationnew.java
+++ src/main/java/cook/SecurityConfigurationnew.java
@@ -17,7 +17,7 @@ public class SecurityConfigurationnew {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                // .sessionCreationPolicy(SessionCreationPolicy.NEVER)
             .and().authorizeRequests()
                 .antMatchers("/api/v1/session/**",
                              "/swagger-ui/**",
